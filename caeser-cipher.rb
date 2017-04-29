b4e870c1302781b1ab1600487bdf6f9d3a4fb68e
Colin Richardson <richardsonjcolin@gmail.com>
2017-04-29 11:50:59 +0900

    Completely functional

--- caeser-cipher.rb
+++ caeser-cipher.rb
@@ -1,34 +1,42 @@
-
+puts "What would you like to incript?\nPlease type here: "
 user_input = gets.chomp
+puts "And by how much would you like to shift the letters?\nAnything from 1-25: "
 shift = gets.chomp.to_i
 
-/def strToArray
-	user_input = user_input.split(" ")
-	user_input.each {|c| c.replace c.ord.to_s}
-	user_input.map! {|n| n.to_i}
-end/
+def caeser_cipher(user_input, shift)
 
-def caeser_cipher(string, num)
-	caps_num = 65.upto(90).to_a
-  lowcase_num = 97.upto(122).to_a
+	string = user_input
+	num = shift
 
-	string = string.split("")
-	string.each {|c| c.replace c.ord.to_s}
-	string.map! {|n| n.to_i}
+	string.each_char do |c|
+		x = c.ord
 
-	string.map! do |n|
-		if lowcase_num.index{|d| n == d}
-			n += num		
-		elsif caps_num.index{|d| n == d}
-			n += num
-		else
-			n = n
+	  # Between 65-90 are lowercase alphabet in .ord. 
+	  if x >= 65 && x <= 90
+	  	x += num % 26
+
+	  	# Wraps back to "a".
+	  	unless x < 91
+	  		x -= 26
+	  	end
+
+	  # Between 97-122 are uppercase alphabet in .ord.
+	  elsif x >= 97 && x <= 122
+	  	x += num % 26
+
+	  	# Wraps back to "A".
+	  	unless x < 123
+	  		x -= 26	  		
+	  	end
+
+	  else
+	  	x = x
 		end
-	end
 
-	string.map!{|n| n.chr}
-	string = string.join
-	puts string
+		print x.chr
+			
+	end
+	
 end
 
 caeser_cipher(user_input, shift)
